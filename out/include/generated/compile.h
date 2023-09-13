8e13ee67b7c8eeecd157f4681b206f8a6ae07abd
Yuzuru Ryu <mtbae034@gmail.com>
2023-09-13 05:26:15 +0000

    build

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Sep 13 05:12:46 UTC 2023"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "yndzx-sekizokernel-j873f9skimh"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.9.337-Sekizo-KSU-OC.Version-Rev1.1/@Ryuzu+"
